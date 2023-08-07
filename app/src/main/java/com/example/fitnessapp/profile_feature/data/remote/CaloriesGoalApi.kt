f84bbb48a6e5cfccdd030b60e787da2765d84fe2
sebastianmartii <sebastian.marzec02@gmail.com>
2023-08-07 19:30:12 +0200

    delete unused imports

--- app/src/main/java/com/example/fitnessapp/profile_feature/data/remote/CaloriesGoalApi.kt
+++ app/src/main/java/com/example/fitnessapp/profile_feature/data/remote/CaloriesGoalApi.kt
@@ -1,10 +1,8 @@
 package com.example.fitnessapp.profile_feature.data.remote
 
-import com.example.fitnessapp.core.util.ApiKeys
 import com.example.fitnessapp.profile_feature.data.remote.dto.CaloriesRequirementsDto
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface CaloriesGoalApi {
