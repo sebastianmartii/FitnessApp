0659815a9280bb2bef83b864f4702c9daa9a56ad
sebastianmartii <sebastian.marzec02@gmail.com>
2023-08-09 20:09:30 +0200

    add nutrition calculator api key to build config

--- app/src/main/java/com/example/fitnessapp/core/util/ApiKeys.kt
+++ app/src/main/java/com/example/fitnessapp/core/util/ApiKeys.kt
@@ -3,6 +3,6 @@ package com.example.fitnessapp.core.util
 import com.example.fitnessapp.BuildConfig
 
 object ApiKeys {
-
     const val FITNESS_CALCULATOR_API_KEY = BuildConfig.FITNESS_CALCULATOR_API_KEY
+    const val NUTRITION_CALCULATOR_API_KEY = BuildConfig.NUTRITION_CALCULATOR_API_KEY
 }
