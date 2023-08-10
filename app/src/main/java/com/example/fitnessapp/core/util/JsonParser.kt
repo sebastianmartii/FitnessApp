5bad9c707bc9113a57285e35e629601e3af7166d
sebastianmartii <sebastian.marzec02@gmail.com>
2023-08-10 15:53:53 +0200

    add json and gson parsers

---
+++ app/src/main/java/com/example/fitnessapp/core/util/JsonParser.kt
@@ -0,0 +1,11 @@
+package com.example.fitnessapp.core.util
+
+import java.lang.reflect.Type
+
+
+interface JsonParser {
+
+    fun <T> fromJson(json: String, type: Type): T?
+
+    fun <T> toJson(obj: T, type: Type): String?
+}
