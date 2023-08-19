74c8a59370a1c44c4cb5ef08904be1f318c9a89f
sebastianmartii <sebastian.marzec02@gmail.com>
2023-08-19 20:36:43 +0200

    add recipe api key and app id to build config

--- app/src/main/java/com/example/fitnessapp/core/util/ApiKeys.kt
+++ app/src/main/java/com/example/fitnessapp/core/util/ApiKeys.kt
@@ -5,4 +5,6 @@ import com.example.fitnessapp.BuildConfig
 object ApiKeys {
     const val FITNESS_CALCULATOR_API_KEY = BuildConfig.FITNESS_CALCULATOR_API_KEY
     const val NUTRITION_CALCULATOR_API_KEY = BuildConfig.NUTRITION_CALCULATOR_API_KEY
+    const val RECIPES_API_KEY = BuildConfig.RECIPES_API_KEY
+    const val RECIPES_AP_ID = BuildConfig.RECIPES_AP_ID
 }
