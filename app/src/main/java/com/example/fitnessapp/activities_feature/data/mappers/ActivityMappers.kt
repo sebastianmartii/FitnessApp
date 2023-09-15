f19b9d1c358920f6c07083f1e8736dcd8eaab5f4
sebastianmartii <sebastian.marzec02@gmail.com>
2023-09-15 19:55:17 +0200

    add to duration mapper for changing double values to duration double, implement it in to duration string mapper to operate this mapping logic only on numbers

--- app/src/main/java/com/example/fitnessapp/activities_feature/data/mappers/ActivityMappers.kt
+++ app/src/main/java/com/example/fitnessapp/activities_feature/data/mappers/ActivityMappers.kt
@@ -5,7 +5,9 @@ import com.example.fitnessapp.activities_feature.data.remote.dto.Data
 import com.example.fitnessapp.activities_feature.domain.model.Activity
 import com.example.fitnessapp.activities_feature.domain.model.SavedActivity
 import com.example.fitnessapp.activities_feature.presentation.ActivitiesTabRowItem
+import com.example.fitnessapp.core.util.toDuration
 import com.example.fitnessapp.daily_overview_feature.data.local.entity.DailyActivitiesEntity
+import kotlin.math.roundToInt
 
 fun Data.toActivityList(): Activity {
     return Activity(
@@ -31,20 +33,24 @@ fun ActivitiesTabRowItem.toTabTitle(): String {
     }
 }
 
-fun Double.toDurationString(): String {
-    val splitDuration = this.toString().split(".")
-    val suffix: String = when {
-        this % 1.0 == 0.0 -> {
-            "min"
+fun Double.toDurationString(isDuration: Boolean = false): String {
+    val duration = if (isDuration) this else this.toDuration()
+    val minutes = duration.toInt()
+    val seconds = (((duration % 1.0) * 0.6) * 100.0).roundToInt()
+    return when {
+        seconds == 0 -> {
+          "${minutes}min"
         }
-        this < 0.60 -> {
-            "s"
+        minutes == 0 -> {
+            "${seconds}s"
         }
-        else -> ""
+        seconds == 60 -> {
+            "${minutes + 1}min"
+        }
+        else -> {
+            "$minutes:$seconds"
+        }
     }
-    val decimal = splitDuration[1]
-    val additionalZero = if (decimal.length == 1 && decimal != "0") "0" else ""
-    return "${splitDuration.filter { it != "0" }.joinToString(":")}$additionalZero$suffix"
 }
 
 fun String.toCaloriesString(): String {
--- app/src/main/java/com/example/fitnessapp/core/util/Duration.kt
+++ app/src/main/java/com/example/fitnessapp/core/util/Duration.kt
@@ -3,3 +3,9 @@ package com.example.fitnessapp.core.util
 fun duration(minutes: String, seconds: String): Double {
     return minutes.toDouble() + (seconds.toDouble()) / 100
 }
+
+fun Double.toDuration(): Double {
+    val minutes = this.toInt() * 0.6
+    val seconds = this % 1.0
+    return (minutes + seconds)/0.6
+}
