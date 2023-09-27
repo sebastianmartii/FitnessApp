69e7f60aeba50cfab5d7f4ec735a0c797951427e
sebastianmartii <sebastian.marzec02@gmail.com>
2023-09-27 19:14:58 +0200

    delete redundant experimental modifier

--- app/src/main/java/com/example/fitnessapp/history_feature/presentation/HistoryScreen.kt
+++ app/src/main/java/com/example/fitnessapp/history_feature/presentation/HistoryScreen.kt
@@ -3,7 +3,6 @@ package com.example.fitnessapp.history_feature.presentation
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -93,7 +92,7 @@ fun HistoryScreen(
     )
 }
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HistoryScreenContent(
     currentMont: String,
