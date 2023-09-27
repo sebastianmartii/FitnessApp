68e32b5871ed6fd078826f07c364bf6ad1051cac
sebastianmartii <sebastian.marzec02@gmail.com>
2023-09-27 19:15:59 +0200

    change activities details visibility to visible when activities are not empty, add empty screen icon

--- app/src/main/java/com/example/fitnessapp/history_feature/presentation/HistoryDetailsScreen.kt
+++ app/src/main/java/com/example/fitnessapp/history_feature/presentation/HistoryDetailsScreen.kt
@@ -2,15 +2,18 @@ package com.example.fitnessapp.history_feature.presentation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -25,6 +28,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
@@ -60,15 +64,17 @@ fun HistoryDetailsScreen(
             )
         }
     ) { paddingValues ->
-        VerticalPager(
+        HorizontalPager(
             state = pagerState,
             pageContent = {page ->
                 HistoryDetailsPageContent(
-                    performedActivities = state.currentMonthActivities.filter { it.day == page },
-                    nutrition = mapMealToMealDetails(state.currentMonthNutrition, page)
+                    performedActivities = state.currentMonthActivities.filter { it.day == (page + 1) },
+                    nutrition = mapMealToMealDetails(state.currentMonthNutrition, (page + 1))
                 )
             },
+            verticalAlignment = Alignment.Top,
             modifier = Modifier
+                .fillMaxSize()
                 .padding(paddingValues)
         )
     }
@@ -85,8 +91,20 @@ private fun HistoryDetailsPageContent(
         verticalArrangement = Arrangement.Top,
         modifier = modifier
             .verticalScroll(rememberScrollState())
-            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth()
     ) {
+        if (performedActivities.isEmpty() && nutrition.isEmpty()) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Icon(
+                    painter = painterResource(id = R.drawable.sentiment_stressed_24px),
+                    contentDescription = stringResource(id = R.string.history_details_empty_screen_text),
+                    modifier = Modifier
+                        .size(48.dp)
+                        .align(Alignment.Center)
+                )
+            }
+        }
         nutrition.onEach { mealDetails ->
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -155,35 +173,38 @@ private fun HistoryDetailsPageContent(
                 .fillMaxWidth()
                 .padding(horizontal = 24.dp)
         )
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .padding(horizontal = 32.dp)
-        ) {
-            Column {
-                Text(
-                    text = stringResource(id = R.string.burned_calories_label),
-                    style = MaterialTheme.typography.labelMedium.copy(
-                        fontStyle = FontStyle.Italic
+        if (performedActivities.isNotEmpty()) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(horizontal = 32.dp)
+            ) {
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.burned_calories_label),
+                        style = MaterialTheme.typography.labelMedium.copy(
+                            fontStyle = FontStyle.Italic
+                        )
                     )
-                )
-                Text(
-                    text = performedActivities.sumOf { it.burnedCalories.toInt() }.toString(),
-                    style = MaterialTheme.typography.titleLarge
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Column {
-                Text(
-                    text = stringResource(id = R.string.duration_label),
-                    style = MaterialTheme.typography.labelMedium.copy(
-                        fontStyle = FontStyle.Italic
+                    Text(
+                        text = performedActivities.sumOf { it.burnedCalories.toInt() }.toString(),
+                        style = MaterialTheme.typography.titleLarge
                     )
-                )
-                Text(
-                    text = performedActivities.sumOf { it.performedActivitiesDuration.toDuration() }.toDurationString(true),
-                    style = MaterialTheme.typography.titleLarge
-                )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.duration_label),
+                        style = MaterialTheme.typography.labelMedium.copy(
+                            fontStyle = FontStyle.Italic
+                        )
+                    )
+                    Text(
+                        text = performedActivities.sumOf { it.performedActivitiesDuration.toDuration() }
+                            .toDurationString(true),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
             }
         }
     }
