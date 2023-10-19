603e7da21301cc71cffe1f43d66cf8e2be3bbdab
sebastianmartii <sebastian.marzec02@gmail.com>
2023-10-19 19:31:26 +0200

    add profile destination

--- app/src/main/java/com/example/fitnessapp/core/navigation/NavigationDrawerDestinations.kt
+++ app/src/main/java/com/example/fitnessapp/core/navigation/NavigationDrawerDestinations.kt
@@ -3,4 +3,5 @@ package com.example.fitnessapp.core.navigation
 sealed class NavigationDrawerDestinations(val route: String) {
 
     object History : NavigationDrawerDestinations("history")
+    object Profile : NavigationDrawerDestinations("profile")
 }
