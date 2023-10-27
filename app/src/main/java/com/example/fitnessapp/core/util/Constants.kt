1e0c5993dcdff8e9412d0097e5d4df5b57eb9daf
sebastianmartii <sebastian.marzec02@gmail.com>
2023-10-27 20:34:13 +0200

    move activity levels list to constants, delete redundant constants

--- app/src/main/java/com/example/fitnessapp/core/util/Constants.kt
+++ app/src/main/java/com/example/fitnessapp/core/util/Constants.kt
@@ -1,26 +1,12 @@
 package com.example.fitnessapp.core.util
 
-val calendarYear = mapOf(
-    1 to "January",
-    2 to "February",
-    3 to "March",
-    4 to "April",
-    5 to "May",
-    6 to "June",
-    7 to "July",
-    8 to "August",
-    9 to "September",
-    10 to "October",
-    11 to "November",
-    12 to "December",
-)
+import com.example.fitnessapp.profile_feature.presentation.sign_in.ActivityLevel
 
-val days = listOf(
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-    "Sun"
+val activityLevels = listOf(
+    ActivityLevel.LEVEL_1,
+    ActivityLevel.LEVEL_2,
+    ActivityLevel.LEVEL_3,
+    ActivityLevel.LEVEL_4,
+    ActivityLevel.LEVEL_5,
+    ActivityLevel.LEVEL_6,
 )
