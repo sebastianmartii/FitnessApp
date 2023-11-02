a1d197fae6a948880c41818e1f6228d59eadce68
sebastianmartii <sebastian.marzec02@gmail.com>
2023-11-02 20:17:50 +0100

    add profile state properties for a list of calculated calories, age, height weight and calories goal validators, showing user update dialog and save user top bar action

--- app/src/main/java/com/example/fitnessapp/profile_feature/presentation/profile/ProfileState.kt
+++ app/src/main/java/com/example/fitnessapp/profile_feature/presentation/profile/ProfileState.kt
@@ -1,5 +1,6 @@
 package com.example.fitnessapp.profile_feature.presentation.profile
 
+import com.example.fitnessapp.profile_feature.domain.model.CalculatedCalories
 import com.example.fitnessapp.profile_feature.domain.model.Gender
 import com.example.fitnessapp.profile_feature.presentation.sign_in.ActivityLevel
 
@@ -12,5 +13,13 @@ data class ProfileState(
     val height: String = "",
     val activityLevel: ActivityLevel = ActivityLevel.LEVEL_0,
     val genderExpanded: Boolean = false,
-    val activityLevelExpanded: Boolean = false
+    val calculatedCalories: List<CalculatedCalories> = emptyList(),
+    val activityLevelExpanded: Boolean = false,
+    val isSaveUserActionVisible: Boolean = false,
+    val isUserUpdateDialogVisible: Boolean = false,
+    val isAgeValid: Boolean = true,
+    val isHeightValid: Boolean = true,
+    val isWeightValid: Boolean = true,
+    val isCaloriesGoalValid: Boolean = true,
+    val shouldValidatorsUpdate: Boolean = false
 )
