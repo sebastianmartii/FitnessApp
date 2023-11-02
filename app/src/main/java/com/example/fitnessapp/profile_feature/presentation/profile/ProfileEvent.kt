6e945fc823081a9152ca5f5c8035bb563e659fb2
sebastianmartii <sebastian.marzec02@gmail.com>
2023-11-02 20:15:51 +0100

    add isValid arguments to events for changing age, weight, height and calories goal, delete redundant events, add events for showing and dismissing user update dialog and for accepting and declining changes in current user

--- app/src/main/java/com/example/fitnessapp/profile_feature/presentation/profile/ProfileEvent.kt
+++ app/src/main/java/com/example/fitnessapp/profile_feature/presentation/profile/ProfileEvent.kt
@@ -5,14 +5,23 @@ import com.example.fitnessapp.profile_feature.presentation.sign_in.ActivityLevel
 
 sealed interface ProfileEvent {
     data class OnUserNameChange(val newUserName: String) : ProfileEvent
-    data class OnAgeChange(val newAge: String) : ProfileEvent
-    data class OnWeightChange(val newWeight: String) : ProfileEvent
-    data class OnHeightChange(val newHeight: String) : ProfileEvent
+    data class OnAgeChange(val newAge: String, val isAgeValid: Boolean) : ProfileEvent
+    data class OnWeightChange(val newWeight: String, val isWeightValid: Boolean) : ProfileEvent
+    data class OnHeightChange(val newHeight: String, val isHeightValid: Boolean) : ProfileEvent
     data class OnGenderChange(val newGender: Gender) : ProfileEvent
     data class OnActivityLevelChange(val newActivityLevel: ActivityLevel) : ProfileEvent
-    data class OnCaloriesGoalChange(val newCaloriesGoal: String) : ProfileEvent
+    data class OnCaloriesGoalChange(val newCaloriesGoal: String, val isCaloriesGoalValid: Boolean) : ProfileEvent
     object OnCaloriesGoalCalculate : ProfileEvent
-    data class OnCaloriesGoalSelect(val selectedCaloriesGoal: String) : ProfileEvent
     data class OnGenderExpandedChange(val expanded: Boolean) : ProfileEvent
     data class OnActivityLevelExpandedChange(val expanded: Boolean) : ProfileEvent
+    object OnUserUpdateDialogDismiss : ProfileEvent
+    object OnUserUpdateDialogDecline : ProfileEvent
+    data class OnUserUpdateDialogShow(val pendingNavigationRoute: String) : ProfileEvent
+    data class OnUserUpdate(
+        val state: ProfileState,
+        val isAgeValid: Boolean,
+        val isHeightValid: Boolean,
+        val isWeightValid: Boolean,
+        val isCaloriesGoalValid: Boolean,
+    ) : ProfileEvent
 }
