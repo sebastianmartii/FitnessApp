f0e18af46f6cc472c5e14e38a55321e8f44a68d5
sebastianmartii <sebastian.marzec02@gmail.com>
2023-12-10 18:59:39 +0100

    delete redundant event and update other events as needed

--- app/src/main/java/com/example/fitnessapp/nutrition_calculator_feature/presentation/recipe/RecipeSearchEvent.kt
+++ app/src/main/java/com/example/fitnessapp/nutrition_calculator_feature/presentation/recipe/RecipeSearchEvent.kt
@@ -6,8 +6,7 @@ sealed interface RecipeSearchEvent {
 
     data class OnQueryChange(val query: String) : RecipeSearchEvent
     data class OnIsSearchBarActiveChange(val isActive: Boolean) : RecipeSearchEvent
-    data class OnNavigateToRecipeDetails(val recipeIndex: Int) : RecipeSearchEvent
-    data class OnIsRecipeSavedChange(val recipe: Recipe, val isSaved: Boolean) : RecipeSearchEvent
+    data class OnNavigateToRecipeDetails(val recipe: Recipe) : RecipeSearchEvent
     object OnRecipeSearch : RecipeSearchEvent
-    object OnTextFieldClear : RecipeSearchEvent
+    object OnQueryClear : RecipeSearchEvent
 }
